a1071a635b5c5b96662cf0717c77be01ec7edfeb
Jason Liu <jason.liu@oocl.com>
2018-11-21 14:45:00 +0800

    loop on mapping

--- practices/filter/grouping_count.js
+++ practices/filter/grouping_count.js
@@ -1,8 +1,20 @@
 'use strict';
 
 function grouping_count(collection) {
-
-  //implement here
+/*	var result = [];
+	
+	if 
+	for (let index = 0; index < collection.length ; index ++){
+		for (let j = 0 ; j < result.length ; j++){
+			if (collection[i] === result[j]){
+				
+			}
+		}
+	}
+ */ 
+}
+function addCount(num){
+	return num++;
 }
 
 module.exports = grouping_count;
--- practices/map/map_to_even.js
+++ practices/map/map_to_even.js
@@ -1,5 +1,12 @@
 'use strict';
 function map_to_even(collection){
-  return [2,4,6,8,10];
+	for (let index = 0 ; index< collection.length;index++){
+		collection[index] = timesTwo(collection[index]);
+	}
+	return collection;
+}
+
+function timesTwo(num){
+	return num*2;
 }
 module.exports = map_to_even;
