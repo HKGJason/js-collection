3fe77b595f41f7f3e1f83bc75d04c9cb6617b685
Jason Liu <jason.liu@oocl.com>
2018-11-21 14:07:38 +0800

    chose multi of 3

--- practices/filter/choose_multiples_of_three.js
+++ practices/filter/choose_multiples_of_three.js
@@ -2,7 +2,13 @@
 
 function choose_multiples_of_three(collection) {
 
-  //implement here
+  var result = [];
+  collection.forEach(function(element){
+	  if(element % 3 === 0)
+		  result.push(element);
+  });
+  return result;
+  
 }
 
 module.exports = choose_multiples_of_three;
