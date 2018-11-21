dab3683764a47c0b5164fb491342bcefd138fbc8
Jason Liu <jason.liu@oocl.com>
2018-11-21 14:55:36 +0800

    map even

--- practices/map/map_to_even.js
+++ practices/map/map_to_even.js
@@ -1,12 +1,18 @@
 'use strict';
+
+
 function map_to_even(collection){
+	/*
 	for (let index = 0 ; index< collection.length;index++){
 		collection[index] = timesTwo(collection[index]);
 	}
-	return collection;
+	*/
+	return collection.map(timesTwo);
+	
 }
-
 function timesTwo(num){
 	return num*2;
 }
+
+
 module.exports = map_to_even;
