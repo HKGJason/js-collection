fe744cbc7e8fd54a3731b4c5cc303a17c302f3bd
Jason Liu <jason.liu@oocl.com>
2018-11-21 14:07:58 +0800

    chose multi of 2

--- practices/filter/choose_even.js
+++ practices/filter/choose_even.js
@@ -2,7 +2,12 @@
 
 function choose_even(collection) {
 
-  //implement here
+  var result = [];
+  collection.forEach(function(element){
+	  if(element % 2 === 0)
+		  result.push(element);
+  });
+  return result;
 }
 
 module.exports = choose_even;
